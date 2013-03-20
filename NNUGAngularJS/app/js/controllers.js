aba413bbe42f0278ca70777f43266c58c97d559e
Glenn F. Henriksen <glenn@henriksen.no>
2013-03-20 14:05:39 +0100

    Filter, length and title example

--- NNUGAngularJS/app/js/controllers.js
+++ NNUGAngularJS/app/js/controllers.js
@@ -1,13 +1,25 @@
 ﻿function TaskCtrl($scope) {
     $scope.tasks = [
-        {
-            "name": "Do stuff",
-        },
-        {
-            "name": "Do more stuff",
-        },
-        {
-            "name": "Do less stuff",
-        }
+        { "name": "Buy a condom, a pregnancy test and a pair of baby shoes at a store to freak out the cashier." },
+        { "name": "Run into a store and ask the year. When someone answers, yell \"It worked!\" and run out cheering." },
+        { "name": "Go to a store, and leave a trail of orange juice leading to the bathrooms." },
+        { "name": "Go to pet store and put on a collar when people walk by say adopt me " },
+        { "name": "Go to a store and sing the \"lalaloopsi\" song!" },
+        { "name": "Go around the store on your best friend's back." },
+        { "name": "Run into a store, ask what year it is. When someone answer yell, \"IT WORKED\" and run out cheering." },
+        { "name": "Go to a mattress store and pretend to be asleep on a mattress." },
+        { "name": "Follow someone in a store or public place and laugh everytime they pick up something." },
+        { "name": "Steal a tomato in a grocery store, bring it home and cut it open READ NEXT ONE." },
+        { "name": "Go to a store and buy an adult diaper and give it to a random person." },
+        { "name": "Go to a store and ask where their throwing spoons are." },
+        { "name": "Go into a store and ask the guy running it what year it is. after he answers yell, YES IT WORKED!" },
+        { "name": "Go to the ice cream store and ask for a sample of every flavor, than leave." },
+        { "name": "Hide in the suitcases at department stores. Pop out and yell potatoes when you hear people coming." },
+        { "name": "Go to the apple store and ask for a kiwi phone." },
+        { "name": "Go into a store and demand to see their mascot." },
+        { "name": "Walk into a crowded store and yell \"Shamwow!\"." },
+        { "name": "Go to the store get a pineapple and ask a random person if sponge bobs in there." },
+        { "name": "Follow strangers around the department store and laugh hysterically every time they touch anything." },
+        { "name": "Squirt ketchup in someones face at the store and say would you like some fries and hold up fries." }
     ];
 }
